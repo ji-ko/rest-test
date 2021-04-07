58842f9ef87b8473a1d1bfe690b35ba39c1bd9d0
Juhani K <yamaimo@outlook.com>
2021-04-07 11:47:26 +0300

    require Review model, add new post route

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Review = require("../models/Review");
 
 router.get("/", (req, res) => {
   res.send("Reviews");
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 module.exports = router;
