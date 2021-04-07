9168847b43975cfb3bb332a9b744453e8f6f6e32
Juhani K <yamaimo@outlook.com>
2021-04-07 10:59:43 +0300

    Add dependency (dotenv), require (mongoose), DB connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Index");
 });
+
+mongoose.connect(
+  "mongodb+srv://<user>:<pwd>@<url>/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true },
+  () => {
+    console.log("Connected to database");
+  }
+);
 
 app.listen(3333, () => console.log("Server started"));
