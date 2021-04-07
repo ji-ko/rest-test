9ea06b0098d953caa815f7295c3628603de65136
Juhani K <yamaimo@outlook.com>
2021-04-07 14:49:25 +0300

    Simplify post route's saving by using async and await instead of promise

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,24 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Review = require("../models/Review");
 
-router.get("/", (req, res) => {
-  res.send("Reviews");
+router.get("/", async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.json(reviews);
+  } catch (err) {
+    res.json({ errorMessage: err });
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const review = new Review({
     kanji: req.body.kanji,
     kana: req.body.kana,
   });
 
-  review
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+  const savedReview = await review.save();
+  res.json(savedReview);
 });
 
 module.exports = router;
